8f3587b39040f0705c7286b5e94b9f5fb7949623
Marco Antônio Putrino Martins <81440774+Marcoantonio-2001@users.noreply.github.com>
2021-06-24 14:43:43 -0300

    Add files via upload

---
+++ Programa.java
@@ -0,0 +1,10 @@
+public class Programa {
+
+    public static void main(String[] args) {
+
+        Onibus o = new Onibus("Johnathan", new ArrayList[] { "teste", "teste" }, 40, 40);
+
+        System.out.println("A" + o.paradas);
+    }
+
+}
---
+++ Programa_elevador.java
@@ -0,0 +1,7 @@
+public class Programa_elevador {
+
+    public static void main(String[] args) {
+
+    }
+
+}
